46ae7082524dfa7dd00064d2adb82aaff5786971
pablo0261 <pablobesler@gmail.com>
2024-01-14 10:49:00 -0300

    adicionando georef

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,11 +1,15 @@
 const { Router } = require('express');
 const dogsRouter = require('./dogsRouter');
 const temperamentsRouter = require('./temperamentsRouter');
+const georefRouter = require('./georefRouter');
 
 
 const routes = Router();
-
+//*DOGS
 routes.use("/dogs", dogsRouter); 
 routes.use("/temperaments", temperamentsRouter); 
+
+//* GEOREF
+routes.use("/georef",georefRouter);
 
 module.exports = routes; 
